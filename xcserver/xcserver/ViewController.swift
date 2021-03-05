0afbff32e03ab1d1a48f4b6d528c15b23ad0684c
Denys Litvinskyi <litvinskiidev@gmail.com>
2021-03-05 18:03:54 +0200

    Setup project

---
+++ xcserver/xcserver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcserver
+//
+//  Created by Denys Litvinskyi on 05.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
